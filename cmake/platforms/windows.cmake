733606c2b297267016de41cbaf4ea1255b805386
Joe Jordan <dev@ignitesoft.com>
2026-09-23 15:39:34 -0400

    Take the QVM tools' executable suffix from the host, not the target
    The QVM tools run on the build machine. Cross compiling for Windows on
    Linux looked for q3lcc.exe, which doesn't exist there; building for a
    non-Windows target on Windows (e.g. Emscripten) had the opposite problem.

--- cmake/platforms/windows.cmake
+++ cmake/platforms/windows.cmake
@@ -40,11 +40,6 @@ endif()
 
 set(CLIENT_EXECUTABLE_OPTIONS WIN32)
 
-# It's only necessary to set this on Windows; elsewhere
-# CMAKE_EXECUTABLE_SUFFIX will be empty anyway, or we want
-# HOST_EXECUTABLE_SUFFIX to be empty for other reasons
-set(HOST_EXECUTABLE_SUFFIX ${CMAKE_EXECUTABLE_SUFFIX})
-
 set(CPACK_GENERATOR NSIS)
 set(CPACK_NSIS_MUI_ICON ${WINDOWS_ICON_PATH})
 set(CPACK_NSIS_EXECUTABLES_DIRECTORY .)
